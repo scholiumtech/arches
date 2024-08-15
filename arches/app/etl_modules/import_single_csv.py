24c4b3b35eb5dc059fb91ea9def29067989c9b6c
Galen <whatisgalen@gmail.com>
2024-08-14 23:54:03 -0700

    fix non-ascii chars in csv using django.default_storage re #11337

--- arches/app/etl_modules/import_single_csv.py
+++ arches/app/etl_modules/import_single_csv.py
@@ -1,4 +1,5 @@
 import csv
+import io
 from datetime import datetime
 import json
 import os
@@ -116,8 +117,9 @@ class ImportSingleCsv(BaseImportModule):
                 "message": _("Upload a valid csv file"),
             }
 
-        with default_storage.open(csv_file_path, mode="r") as csvfile:
-            reader = csv.reader(csvfile)
+        with default_storage.open(self.csv_file_path, mode="rb") as csvfile:
+            text_wrapper = io.TextIOWrapper(csvfile, encoding="utf-8")
+            reader = csv.DictReader(text_wrapper)
             data = {"csv": [line for line in reader], "csv_file": csv_file_name}
             with connection.cursor() as cursor:
                 cursor.execute(
@@ -300,10 +302,9 @@ class ImportSingleCsv(BaseImportModule):
     ):
         temp_dir = os.path.join(settings.UPLOADED_FILES_DIR, "tmp", loadid)
         csv_file_path = os.path.join(temp_dir, csv_file_name)
-        with default_storage.open(csv_file_path, mode="r") as csvfile:
-            reader = csv.reader(
-                csvfile
-            )  # if there is a duplicate field, DictReader will not work
+        with default_storage.open(csv_file_path, mode="rb") as csvfile:
+            text_wrapper = io.TextIOWrapper(csvfile, encoding="utf-8")
+            reader = csv.reader(text_wrapper)
 
             if has_headers:
                 next(reader)
